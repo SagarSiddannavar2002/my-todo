38b32b5a5b59d1b067e6f83044669b8a621993c6
SagarSiddannavar2002 <sagar.s@aveenyasolutions.com>
2026-01-07 17:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,53 +3,46 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app2.db'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    
 
     def __repr__(self):
         return f'<Todo {self.id} - {self.title}>'
 
-# Routes
-@app.route('/' ,methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=='POST':
-        print(request.form["title"])
-        title=request.form["title"]
-        
-        todo= Todo(title=title)
+    if request.method == 'POST':
+        title = request.form['title']
+        todo = Todo(title=title)
         db.session.add(todo)
         db.session.commit()
-            
+
     todos = Todo.query.all()
-    return render_template('index.html' , todos=todos)
-@app.route('/delete <int:id>')
+    return render_template('index.html', todos=todos)
+
+@app.route('/delete/<int:id>')
 def delete(id):
-    todo=Todo.query.get(id)
+    todo = Todo.query.get_or_404(id)
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
-    
 
-@app.route('/update <int:id>', methods=['GET', 'POST'])
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    if request.method=='POST':
-        title=request.form["title"]
-        todo=Todo.query.get(id)
-        todo.title=title
+    todo = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        todo.title = request.form['title']
         db.session.commit()
         return redirect('/')
-    todo = Todo.query.get(id)   
     return render_template('update.html', todo=todo)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run()
