44edfe6a99fe0ede86d7a4e8d7d122a3521b892b
SagarSiddannavar2002 <sagar.s@aveenyasolutions.com>
2026-01-07 18:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import os
 
 app = Flask(__name__)
 
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app2.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -16,14 +18,14 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} - {self.title}>'
 
+# Routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        title = request.form['title']
+        title = request.form["title"]
         todo = Todo(title=title)
         db.session.add(todo)
         db.session.commit()
-
     todos = Todo.query.all()
     return render_template('index.html', todos=todos)
 
@@ -38,16 +40,14 @@ def delete(id):
 def update(id):
     todo = Todo.query.get_or_404(id)
     if request.method == 'POST':
-        todo.title = request.form['title']
+        todo.title = request.form["title"]
         db.session.commit()
         return redirect('/')
     return render_template('update.html', todo=todo)
 
+# Run
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
-
+    app.run(host="0.0.0.0", port=port)
